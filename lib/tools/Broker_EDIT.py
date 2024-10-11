fd253912f0009e7ab1603cf3d8bbea1ffec9ca4d
slaytonmarx <slayton.marx@gmail.com>
2024-10-11 14:38:25 -0400

    added live api back to Broker.py. If someone wants to use it, it's their choice

--- lib/tools/Broker_EDIT.py
+++ lib/tools/Broker_EDIT.py
@@ -2,6 +2,15 @@ import alpaca_trade_api as alpaca
 from slack_sdk import WebClient
 import logging
 
+def live_api(no_message:bool = False):
+    if not no_message: print('Activating with Live')
+    ALPACA_KEY_ID = 'YOUR_API_KEY'
+    ALPACA_SECRET_KEY = 'YOUR_SECRET_KEY'
+    BASE_URL = 'https://api.alpaca.markets'
+    api = alpaca.REST(
+        ALPACA_KEY_ID, ALPACA_SECRET_KEY, base_url=BASE_URL)
+    return api
+
 def paper_api(no_message:bool = False):
     if not no_message: print('Activating with Paper')
     ALPACA_KEY_ID = 'YOUR_API_KEY'
